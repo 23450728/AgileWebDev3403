e991fa58c1436be43bed1ef001d99e893cc287fb
23450728 <133504440+23450728@users.noreply.github.com>
2024-04-15 09:31:12 +0800

    Version 0.1 Basic Flask Application
    Current Version 0.1:
    I have developed some basic flask templates along with a CSS stylesheet. The flask app contains some basic SQLite code that allows for posts to be uploaded. The login page has no functionality. The signup page has no functionality. The main page is the first view a user has, it has some basic functionality, it presents user created posts in a list format. 
    
    Future:
    Login and Signup pages need to connect to the database. When a user signs up, validation needs to be done to check if that user already exists in the database. It then needs to create an account for them. And allow them to sign in. 
    
    The forgot password needs to check if the email/user exists and needs to send a password reset email. 
    
    The profile needs to be updated to contain the ability to change user settings such as username, password, email.
    
    And finally, the main needs to be updated with AJAX code that allows the user generated titles and descriptions to be their own individual post. In addition, editing, liking, deleting and commenting on posts needs to be implemented.  
    
    Highest Priority:
    Ensuring the signup and sign function. I will recommend referring to Flask Web Development by Grinberg, Miguel chapter 2 part 2 because it contains an incredibly useful example of how to implement a flask app.

---
+++ app.py
@@ -0,0 +1,65 @@
+
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+import sqlite3
+
+app = Flask(__name__, template_folder='templates', static_folder='Static')
+socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+connect = sqlite3.connect('database.db') 
+
+connect.execute( 
+    'CREATE TABLE IF NOT EXISTS USER (username TEXT, password TEXT, email TEXT)') 
+
+connect.execute(
+    'CREATE TABLE IF NOT EXISTS POST (title TEXT, description TEXT)'
+)
+
+@app.route('/')
+@app.route('/Main')
+def main():
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM POST')
+
+    data=cursor.fetchall()
+    return render_template('Main.html', data=data)
+
+@app.route('/login')
+def login_page():
+   return render_template('login.html')
+
+
+@app.route('/Create Post', methods=['GET','POST'])
+def Create_post():
+    if request.method == "POST":
+        title = request.form['title']
+        description = request.form['description']
+        with sqlite3.connect("database.db") as POST: 
+            cursor = POST.cursor() 
+            cursor.execute("INSERT INTO POST (title,description) VALUES (?,?)", (title, description)) 
+            POST.commit()
+    return render_template('Create Post.html')
+
+
+@app.route('/Profile')
+def profile():
+    return render_template('Profile.html')
+
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        with sqlite3.connect("database.db") as users: 
+            cursor = users.cursor() 
+            cursor.execute("INSERT INTO USER (username,password,email) VALUES (?,?,?)", (username, password, email)) 
+            users.commit()
+    return render_template('signup.html')
+
+@app.route('/Forgot Password')
+def forgotPassword():
+    return render_template('Forgot Password.html')
