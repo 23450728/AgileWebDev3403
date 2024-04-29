7dc98be2ffbab6cc79d612686e2bea8dd5f6f7bb
Sonj03 <sonajimson@icloud.com>
2024-04-29 20:02:45 +0800

    Make search feature

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 login = LoginManager(app)
 login.login_view = 'login'
 app.config.from_object(Config)
+app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
