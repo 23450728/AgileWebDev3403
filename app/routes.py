1b4a8178aafb41659926c613486c280244033cc0
Kelly <23614821@student.uwa.edu.au>
2024-05-16 20:00:38 +0800

    Attempt to make Test Config, Blueprint, and Factory Function

--- app/routes.py
+++ app/routes.py
@@ -1,58 +1,58 @@
 from flask import render_template, flash, redirect, url_for, request, g
 from flask import render_template, flash, redirect, url_for, request, g
-from app import app, db
+from blueprints import main
 from app.forms import *
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
-from app.models import User, Post, Comment
+from app.models import User, Post,  Comment
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 
-@app.before_request
+@main.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_active = datetime.now(timezone.utc)
         db.session.commit()
     g.search_form = SearchForm()
 
-@app.route('/')
-@app.route('/home')
+@main.route('/')
+@main.route('/home')
 def home():
     return render_template("home.html")
 
-@app.route('/index')
+@main.route('/index')
 def index():
     page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.paginate(query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False)
-    next_url = url_for('index', page=posts.next_num) \
+    posts = db.paginate(query, page=page, per_page=main.config["POSTS_PER_PAGE"], error_out=False)
+    next_url = url_for('main.index', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
+    prev_url = url_for('main.index', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template("index.html", posts=posts.items, next_url=next_url, prev_url=prev_url, explore=True)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@main.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('main.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlsplit(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main.index')
         return redirect(next_page)
     return render_template('login.html', form=form)
 
-@app.route('/register', methods=['GET', 'POST'])
+@main.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -60,28 +60,28 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('register.html', form=form)
 
-@app.route('/logout')
+@main.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
-@app.route('/user/<username>')
+@main.route('/user/<username>')
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
     page = request.args.get('page', 1, type=int)
     query = user.posts.select().order_by(Post.timestamp.desc())
-    posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
+    posts = db.paginate(query, page=page, per_page=main.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('main.user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@main.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def EditProfile():
     form = EditProfileForm()
@@ -90,14 +90,14 @@ def EditProfile():
         current_user.email = form.email.data
         current_user.bio = form.bio.data
         db.session.commit()
-        return redirect(url_for('user', username=current_user.username))
+        return redirect(url_for('main.user', username=current_user.username))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
         form.bio.data = current_user.bio
     return render_template('edit profile.html', form=form)
 
-@app.route('/post', methods=['GET', 'POST'])
+@main.route('/post', methods=['GET', 'POST'])
 @login_required
 def post():
     form = PostForm()
@@ -105,25 +105,25 @@ def post():
         post = Post(title=form.title.data, body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     return render_template("post.html", form=form)
 
 @login_required
-@app.route('/post/<int:id>')
+@main.route('/post/<int:id>')
 def SelectPost(id):
     page = request.args.get('page', 1, type=int)
     prev = request.args.get('prev', '/index')
     post = db.session.scalar(sa.select(Post).where(Post.id == id))
     commentsQuery = sa.select(Comment).where(Comment.post_id == id).order_by(Comment.timestamp)
     comments = db.paginate(commentsQuery, page=page, per_page=10, error_out=False)
-    next_url = url_for('SelectPost', id=id, page=comments.next_num) \
+    next_url = url_for('main.SelectPost', id=id, page=comments.next_num) \
         if comments.has_next else None
-    prev_url = url_for('SelectPost', id=id, page=comments.prev_num) \
+    prev_url = url_for('main.SelectPost', id=id, page=comments.prev_num) \
         if comments.has_prev else None
     return render_template("post view.html", post=post, comments=comments.items, next_url=next_url, prev_url=prev_url, prev=prev)
 
 @login_required
-@app.route('/post/<int:parent>/comment', methods=['GET', 'POST'])
+@main.route('/post/<int:parent>/comment', methods=['GET', 'POST'])
 def AddComment(parent):
     post = db.session.scalar(sa.select(Post).where(Post.id == parent))
     prev = prev = request.args.get('prev', '/index')
@@ -136,7 +136,7 @@ def AddComment(parent):
     return render_template("comment.html", form=form, post=post, prev=prev)
 
 @login_required
-@app.route('/post/<int:parent>/like', methods=['GET', 'POST'])
+@main.route('/post/<int:parent>/like', methods=['GET', 'POST'])
 def LikePost(parent):
     post = db.session.scalar(sa.select(Post).where(Post.id == parent))
     if current_user.is_anonymous:
@@ -151,7 +151,7 @@ def LikePost(parent):
     return redirect('/post/' + str(parent))
 
 @login_required
-@app.route('/post/<int:parent>/like', methods=['GET', 'POST'])
+@main.route('/post/<int:parent>/like', methods=['GET', 'POST'])
 def LikePost(parent):
     post = db.session.scalar(sa.select(Post).where(Post.id == parent))
     if current_user.is_anonymous:
@@ -165,7 +165,7 @@ def LikePost(parent):
         db.session.commit()
     return redirect('/post/' + str(parent))
 
-@app.route('/search')
+@main.route('/search')
 def search():
     searchInput = request.args.get('search')
 
--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class Config:
 
     # export ELASTICSEARCH_URL=http://localhost:9200
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     POSTS_PER_PAGE = 25
+
+class DeploymentConfig(Config): 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+class TestConfig(Config):
+    SQLAlCHEMY_DATABASE_URI = "sqlite:///:memory"
+    TESTING = True
