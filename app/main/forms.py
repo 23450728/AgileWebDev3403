9a715001a755f88debc935e46b0b7cb0cb3c71bc
23450728 <23450728@gmail.com>
2024-05-18 15:17:26 +0800

    Email Server Functional

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,7 +12,6 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
